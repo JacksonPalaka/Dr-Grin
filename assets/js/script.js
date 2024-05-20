9e5c667f837a2324a6b10e9618bb7b29e48989cb
jackson <jacksoncoco29@gmail.com>
2024-05-20 13:14:07 +0530

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,52 +18,6 @@ const addEventOnElem = function (elem, type, callback) {
 
 
 
-/**
- * navbar toggle
- */
-
-const navbar = document.querySelector("[data-navbar]");
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
-const navbarToggler = document.querySelector("[data-nav-toggler]");
-
-const toggleNav = function () {
-  navbar.classList.toggle("active");
-  navbarToggler.classList.toggle("active");
-}
-
-addEventOnElem(navbarToggler, "click", toggleNav);
-
-const closeNav = function () {
-  navbar.classList.remove("active");
-  navbarToggler.classList.remove("active");
-}
-
-addEventOnElem(navbarLinks, "click", closeNav);
-
-
-
-/**
- * header active
- */
-
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-});
-
-
-// video
-
-
-
 // chat on whatsapp
 
 function sendWhatsAppMessage() {
@@ -86,27 +40,3 @@ function makeCall() {
   var phoneNumber = '9403890078';
   window.location.href = 'tel:' + phoneNumber;
 }
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  const videoElement = document.querySelector('.video1');
-  const screenHeight = window.innerHeight;
-
-  window.addEventListener('scroll', function() {
-      const videoRect = videoElement.getBoundingClientRect();
-      const videoTop = videoRect.top;
-      const videoBottom = videoRect.bottom;
-
-      if (videoTop < screenHeight / 2 && videoBottom > screenHeight / 2) {
-          videoElement.play().catch(error => {
-              console.error('Video play error:', error);
-          });
-      } else {
-          videoElement.pause();
-          videoElement.currentTime = 0; // Reset to start when out of view
-      }
-  });
-});
